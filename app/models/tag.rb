94c2c2f68f0e940aa36930c7bd407be73fc84e5e
isaacbrds <isaac.brigido@gmail.com>
2021-09-04 11:04:53 -0300

    Adiciona o modelo TagContent

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,8 @@
 class Tag < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, uniqueness: {scope: :user}
+
+  has_many :tag_contents
+  has_many :contents, through: :tag_contents
 end
